f318457ae86d8d70dde090902e3a63e24ab1c361
Caelum Forder <caelum119@gmail.com>
2018-02-19 17:00:57 +0000

    Moved map over to Angular component

---
+++ floorplan-controllables/src/app/components/map/map.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {d3Floorplan} from '../../../assets/js/d3-floorplan';
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
