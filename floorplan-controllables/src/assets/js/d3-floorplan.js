1b4c4f9acf542d7dd7415f875328cb14ad308154
Unknown <caelum119@gmail.com>
2018-02-21 11:40:52 +0000

    De-minified d3 floormap heatmap method

--- floorplan-controllables/src/assets/js/d3-floorplan.js
+++ floorplan-controllables/src/assets/js/d3-floorplan.js
@@ -139,115 +139,220 @@ d3.floorplan.imagelayer = function() {
     };
     return a
 };
+//
+//   Copyright 2012 David Ciarletta
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
 d3.floorplan.heatmap = function() {
-    function a(a) {
-        a.each(function(a) {
-            if (a && a.map) {
-                var d = d3.select(this);
-                a.units ? " " != a.units.charAt(0) && (a.units = " " + a.units) : a.units = "";
-                var b = a.map.map(function(a) { return a.value }).sort(d3.ascending),
-                    g, j;
-                switch (f) {
-                    case "quantile":
-                        g = d3.scale.quantile().range([1, 2, 3, 4, 5, 6]).domain(b);
-                        j = g.quantiles();
-                        break;
-                    case "quantized":
-                        g = d3.scale.quantize().range([1, 2, 3, 4, 5, 6]).domain([b[0], b[b.length - 1]]);
-                        b = (g.domain()[1] - g.domain()[0]) / 6;
-                        j = [b, 2 * b, 3 * b, 4 * b, 5 * b];
-                        break;
-                    case "normal":
-                        var m =
-                            d3.mean(b),
-                            b = Math.sqrt(d3.sum(b, function(a) { return Math.pow(a - m, 2) }) / b.length);
-                        g = d3.scale.quantile().range([1, 2, 3, 4, 5, 6]).domain([m - 6 * b, m - 2 * b, m - b, m, m + b, m + 2 * b, m + 6 * b]);
-                        j = g.quantiles();
-                        break;
-                    default:
-                        customThresholds || (customThresholds = j), b = customThresholds, b.push(b[b.length - 1]), b.unshift(b[0]), g = d3.scale.quantile().range([1, 2, 3, 4, 5, 6]).domain(b), customThresholds = j = g.quantiles()
-                }
-                d = d.selectAll("g.heatmap").data([0]);
-                d.enter().append("g").attr("class", "heatmap");
-                this.__colors__ && this.__colors__ != e && d.classed(this.__colors__, !1);
-                d.classed(e, !0);
-                this.__colors__ = e;
-                b = d.selectAll("rect").data(a.map.filter(function(a) { return !a.points }), function(a) { return a.x + "," + a.y });
-                j = b.enter().append("rect").style("opacity", 1E-6);
-                b.exit().transition().style("opacity", 1E-6).remove();
-                j.append("title");
-                b.attr("x", function(a) { return i(a.x) }).attr("y", function(a) { return h(a.y) }).attr("height", Math.abs(h(a.binSize) - h(0))).attr("width", Math.abs(i(a.binSize) - i(0))).attr("class", function(a) { return "d6-" + g(a.value) }).select("title").text(function(b) {
-                    return "value: " +
-                        c(b.value) + a.units
-                });
-                j.transition().style("opacity", 0.6);
-                b = d.selectAll("path").data(a.map.filter(function(a) { return a.points }), function(a) { return JSON.stringify(a.points) });
-                j = b.enter().append("path").attr("d", function(a) { return l(a.points) + "Z" }).style("opacity", 1E-6);
-                b.exit().transition().style("opacity", 1E-6).remove();
-                j.append("title");
-                b.attr("class", function(a) { return "d6-" + g(a.value) }).select("title").text(function(b) { return "value: " + c(b.value) + a.units });
-                j.transition().style("opacity", 0.6);
-                d = d.selectAll("text").data(a.map.filter(function(a) { return a.points }),
-                    function(a) { return JSON.stringify(a.points) });
-                b = d.enter().append("text").style("font-weight", "bold").attr("text-anchor", "middle").style("opacity", 1E-6);
-                d.exit().transition().style("opacity", 1E-6).remove();
-                d.attr("transform", function(a) {
-                    for (var b = 0, d = 0, j = 0, g = 0; g < a.points.length; ++g) var c = a.points[g],
-                        k = a.points[g + 1] || a.points[0],
-                        e = c.x * k.y - k.x * c.y,
-                        b = b + (c.x + k.x) * e,
-                        d = d + (c.y + k.y) * e,
-                        j = j + e;
-                    j = j / 2;
-                    d = d / (6 * j);
-                    return "translate(" + i(b / (6 * j)) + "," + h(d) + ")"
-                }).text(function(b) { return c(b.value) + a.units });
-                b.transition().style("opacity",
-                    0.6)
+    var colors = "RdYlBu",
+        scaleType = "quantile",
+        x = d3.scale.linear(),
+        y = d3.scale.linear(),
+        line = d3.svg.line()
+        .x(function(d) { return x(d.x); })
+        .y(function(d) { return y(d.y); }),
+        format = d3.format(".4n"),
+        id = "fp-heatmap-" + new Date().valueOf(),
+        name = "heatmap";
+
+    function heatmap(g) {
+        g.each(function(data) {
+            if (!data || !data.map) return;
+            var g = d3.select(this);
+
+            if (!data.units) {
+                data.units = "";
+            } else if (data.units.charAt(0) != ' ') {
+                data.units = " " + data.units;
             }
-        })
+
+            var values = data.map.map(function(d) { return d.value; })
+                .sort(d3.ascending),
+                colorScale, thresholds;
+
+            switch (scaleType) {
+                case "quantile":
+                    {
+                        colorScale = d3.scale.quantile()
+                        .range([1, 2, 3, 4, 5, 6])
+                        .domain(values);
+                        thresholds = colorScale.quantiles();
+                        break;
+                    }
+                case "quantized":
+                    {
+                        colorScale = d3.scale.quantize()
+                        .range([1, 2, 3, 4, 5, 6])
+                        .domain([values[0], values[values.length - 1]]);
+                        var incr = (colorScale.domain()[1] - colorScale.domain()[0]) /
+                            6;
+                        thresholds = [incr, 2 * incr, 3 * incr, 4 * incr, 5 * incr];
+                        break;
+                    }
+                case "normal":
+                    {
+                        var mean = d3.mean(values);
+                        var sigma = Math.sqrt(d3.sum(values,
+                                function(v) { return Math.pow(v - mean, 2); }) /
+                            values.length);
+                        colorScale = d3.scale.quantile()
+                        .range([1, 2, 3, 4, 5, 6])
+                        .domain([mean - 6 * sigma, mean - 2 * sigma,
+                            mean - sigma, mean, mean + sigma,
+                            mean + 2 * sigma, mean + 6 * sigma
+                        ]);
+                        thresholds = colorScale.quantiles();
+                        break;
+                    }
+                default:
+                    { // custom
+                        if (!customThresholds) customThresholds = thresholds;
+                        var domain = customThresholds;
+                        domain.push(domain[domain.length - 1]);
+                        domain.unshift(domain[0]);
+                        colorScale = d3.scale.quantile()
+                        .range([1, 2, 3, 4, 5, 6])
+                        .domain(domain);
+                        customThresholds = thresholds = colorScale.quantiles();
+                        break;
+                    }
+            }
+
+            // setup container for visualization
+            var vis = g.selectAll("g.heatmap").data([0]);
+            vis.enter().append("g").attr("class", "heatmap");
+
+            if (this.__colors__ && this.__colors__ != colors) {
+                vis.classed(this.__colors__, false);
+            }
+            vis.classed(colors, true);
+            this.__colors__ = colors;
+
+            var cells = vis.selectAll("rect")
+                .data(data.map.filter(function(d) { return !d.points; }),
+                    function(d) { return d.x + "," + d.y; }),
+                cellsEnter = cells.enter().append("rect").style("opacity", 1e-6);
+
+            cells.exit().transition().style("opacity", 1e-6).remove();
+
+            cellsEnter.append("title");
+
+            cells.attr("x", function(d) { return x(d.x); })
+                .attr("y", function(d) { return y(d.y); })
+                .attr("height", Math.abs(y(data.binSize) - y(0)))
+                .attr("width", Math.abs(x(data.binSize) - x(0)))
+                .attr("class", function(d) { return "d6-" + colorScale(d.value); })
+                .select("title")
+                .text(function(d) {
+                    return "value: " + format(d.value) + data.units;
+                });
+
+            cellsEnter.transition().style("opacity", 0.6);
+
+            var areas = vis.selectAll("path")
+                .data(data.map.filter(function(d) { return d.points; }),
+                    function(d) { return JSON.stringify(d.points); }),
+                areasEnter = areas.enter().append("path")
+                .attr("d", function(d) { return line(d.points) + "Z"; })
+                .style("opacity", 1e-6);
+
+            areas.exit().transition().style("opacity", 1e-6).remove();
+            areasEnter.append("title");
+
+            areas
+                .attr("class", function(d) { return "d6-" + colorScale(d.value); })
+                .select("title")
+                .text(function(d) {
+                    return "value: " + format(d.value) + data.units;
+                });
+            areasEnter.transition().style("opacity", 0.6);
+
+            var areaLabels = vis.selectAll("text")
+                .data(data.map.filter(function(d) { return d.points; }),
+                    function(d) { return JSON.stringify(d.points); }),
+                areaLabelsEnter = areaLabels.enter().append("text")
+                .style("font-weight", "bold")
+                .attr("text-anchor", "middle")
+                .style("opacity", 1e-6);
+
+            areaLabels.exit().transition().style("opacity", 1e-6).remove();
+
+            areaLabels.attr("transform", function(d) {
+                    var center = { x: 0, y: 0 };
+                    var area = 0;
+                    for (var i = 0; i < d.points.length; ++i) {
+                        var p1 = d.points[i];
+                        var p2 = d.points[i + 1] || d.points[0];
+                        var ai = (p1.x * p2.y - p2.x * p1.y);
+                        center.x += (p1.x + p2.x) * ai;
+                        center.y += (p1.y + p2.y) * ai;
+                        area += ai;
+                    }
+                    area = area / 2;
+                    center.x = center.x / (6 * area);
+                    center.y = center.y / (6 * area);
+                    return "translate(" + x(center.x) + "," +
+                        y(center.y) + ")";
+                })
+                .text(function(d) { return format(d.value) + data.units; });
+
+            areaLabelsEnter.transition().style("opacity", 0.6);
+        });
     }
-    var e = "RdYlBu",
-        f = "quantile",
-        i = d3.scale.linear(),
-        h = d3.scale.linear(),
-        l = d3.svg.line().x(function(a) { return i(a.x) }).y(function(a) { return h(a.y) }),
-        c = d3.format(".4n"),
-        d = "fp-heatmap-" + (new Date).valueOf(),
-        n = "heatmap";
-    a.xScale = function(d) {
-        if (!arguments.length) return i;
-        i = d;
-        return a
+
+    heatmap.xScale = function(scale) {
+        if (!arguments.length) return x;
+        x = scale;
+        return heatmap;
     };
-    a.yScale = function(d) {
-        if (!arguments.length) return h;
-        h = d;
-        return a
+
+    heatmap.yScale = function(scale) {
+        if (!arguments.length) return y;
+        y = scale;
+        return heatmap;
     };
-    a.colorSet = function(d) {
-        if (!arguments.length) return e;
-        e = d;
-        return a
+
+    heatmap.colorSet = function(scaleName) {
+        if (!arguments.length) return colors;
+        colors = scaleName;
+        return heatmap;
     };
-    a.colorMode = function(d) {
-        if (!arguments.length) return f;
-        f = d;
-        return a
+
+    heatmap.colorMode = function(mode) {
+        if (!arguments.length) return scaleType;
+        scaleType = mode;
+        return heatmap;
     };
-    a.customThresholds =
-        function(d) {
-            if (!arguments.length) return customThresholds;
-            customThresholds = d;
-            return a
-        };
-    a.id = function() { return d };
-    a.title = function(d) {
-        if (!arguments.length) return n;
-        n = d;
-        return a
+
+    heatmap.customThresholds = function(vals) {
+        if (!arguments.length) return customThresholds;
+        customThresholds = vals;
+        return heatmap;
     };
-    return a
+
+    heatmap.id = function() {
+        return id;
+    };
+
+    heatmap.title = function(n) {
+        if (!arguments.length) return name;
+        name = n;
+        return heatmap;
+    };
+
+    return heatmap;
 };
 d3.floorplan.overlays = function() {
     function a(a) {
