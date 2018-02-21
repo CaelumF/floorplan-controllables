2b1975eafbab57828b8e6972d7750c7fd464a643
Unknown <caelum119@gmail.com>
2018-02-21 13:46:59 +0000

    Added drawable lights, drawn from JSON

--- floorplan-controllables/src/app/services/map.service.ts
+++ floorplan-controllables/src/app/services/map.service.ts
@@ -1,56 +1,79 @@
 import { Injectable } from '@angular/core';
+import { debug } from 'util';
 
 @Injectable()
 export class MapService {
-
   constructor() {
     $(function() {
-      console.log("Loaded d3")
       var jsonData = {
-
+        heatmap: {
+          binSize: fcConfig.lightScale,
+          map: fcConfig.currentFloorplan.currentLightConfig.lights
+        }
       };
 
-      var xscale = d3.scale.linear()
+      var xscale = d3.scale
+          .linear()
           .domain([0, 50.0])
           .range([0, 720]),
-          yscale = d3.scale.linear()
+        yscale = d3.scale
+          .linear()
           .domain([0, 33.79])
           .range([0, 487]),
-          map = d3.floorplan().xScale(xscale).yScale(yscale),
-          imagelayer = d3.floorplan.imagelayer(),
-          heatmap = d3.floorplan.heatmap(),
-          vectorfield = d3.floorplan.vectorfield(),
-          pathplot = d3.floorplan.pathplot(),
-          overlays = d3.floorplan.overlays().editMode(true),
-          mapdata = {};
+        map = d3
+          .floorplan()
+          .xScale(xscale)
+          .yScale(yscale),
+        imagelayer = d3.floorplan.imagelayer(),
+        heatmap = d3.floorplan.heatmap(),
+        vectorfield = d3.floorplan.vectorfield(),
+        pathplot = d3.floorplan.pathplot(),
+        overlays = d3.floorplan.overlays().editMode(true),
+        mapdata = {};
 
-      mapdata[imagelayer.id()] = [{
-          url: 'https://dciarletta.github.io/d3-floorplan/Sample_Floorplan.jpg',
+      mapdata[imagelayer.id()] = [
+        {
+          url: fcConfig.currentFloorplan.imageURL,
           x: 0,
           y: 0,
           height: 33.79,
           width: 50.0
-      }];
+        }
+      ];
 
-      map.addLayer(imagelayer)
-          .addLayer(heatmap)
-          .addLayer(vectorfield)
-          .addLayer(pathplot)
-          .addLayer(overlays);
+      // heatmap.colorMode('custom');
+      heatmap.tooltipper = function(d) {
+        return d.status || 'NO STATUS';
+      };
+
+      map
+        .addLayer(imagelayer)
+        .addLayer(heatmap)
+        .addLayer(vectorfield)
+        .addLayer(pathplot)
+        .addLayer(overlays);
 
       var loadData = function(data) {
-          mapdata[heatmap.id()] = data.heatmap;
-          mapdata[overlays.id()] = data.overlays;
-          mapdata[vectorfield.id()] = data.vectorfield;
-          mapdata[pathplot.id()] = data.pathplot;
+        mapdata[heatmap.id()] = data.heatmap;
+        mapdata[overlays.id()] = data.overlays;
+        mapdata[vectorfield.id()] = data.vectorfield;
+        mapdata[pathplot.id()] = data.pathplot;
 
-          d3.select("#demo").append("svg")
-              .attr("height", 487).attr("width", 720)
-              .datum(mapdata).call(map);
+        d3
+          .select('#demo')
+          .append('svg')
+          .attr('height', 487)
+          .attr('width', 720)
+          .datum(mapdata)
+          .call(map);
       };
 
       loadData(jsonData);
-  })
-  }
+      d3.selectAll('.heatmap rect').on('click', (d, i) => {
+        console.log(`Clicked ${JSON.stringify(d)} with ${i}`);
+        const t = d3.select()
+      })
 
+    });
+  }
 }
---
+++ floorplan-controllables/src/assets/js/floorplan-controllable.config.js
@@ -0,0 +1,24 @@
+let fcConfig = {
+    //Size of lights
+    lightScale: 2,
+    floorplans: {
+        house: {
+            imageURL: "https://dciarletta.github.io/d3-floorplan/Sample_Floorplan.jpg",
+            //Locations and properties of lights
+            lightConfigs: {
+                default: {
+                    lights: [
+                        { x: 12, y: 12, id: "KITCHEN_LIGHT1", status: "OFF" },
+                        { x: 32, y: 12, id: "LIVING_ROOM_LIGHT1", status: "OFF" },
+                        { x: 22.2, y: 14.5, id: "LIVING_ROOM_LIGHT2", status: "OFF" },
+                    ],
+                    groups: [
+
+                    ]
+                }
+            },
+            get currentLightConfig() { return this.lightConfigs.default },
+        }
+    },
+    get currentFloorplan() { return this.floorplans.house },
+};
