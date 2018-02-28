39f60b2b0433d2d7b06c7fc642b31b31101257e9
Caelum Forder <caelum119@gmail.com>
2018-02-28 10:26:03 +0000

    Light interface material angular component

---
+++ floorplan-controllables/src/app/components/light-interface/light-interface.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
+@Component({
+  selector: 'app-light-interface',
+  templateUrl: './light-interface.component.html',
+  styleUrls: ['./light-interface.component.css']
+})
+
+export class LightInterfaceComponent implements OnInit {
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit() {
+    
+  }
+
+  open(): void{
+
+  }
+  onNoClick():
+}
